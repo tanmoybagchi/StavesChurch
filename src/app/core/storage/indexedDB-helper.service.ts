c9e35446aa973a5555697276846070203fb16183
Tanmoy Bagchi <tanmoybagchi76@gmail.com>
2018-10-13 07:53:56 -0500

    cleaned up

--- src/app/core/storage/indexedDB-helper.service.ts
+++ src/app/core/storage/indexedDB-helper.service.ts
@@ -1,9 +1,9 @@
 ﻿import { Injectable } from '@angular/core';
 import { Result } from '@app/core/result';
-import { Subject, throwError, Observable } from 'rxjs';
+import { IndexedDBConfigRules } from '@app/core/storage/indexedDB-config-rules';
+import { Observable } from 'rxjs';
 import { IndexedDBConfig } from './indexedDB-config';
 import { StorageConfig } from './storage-config';
-import { IndexedDBConfigRules } from '@app/core/storage/indexedDB-config-rules';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +12,6 @@ export class IndexedDBHelper {
   private configRules = new IndexedDBConfigRules();
 
   constructor(private storageConfig: StorageConfig) {
-    // (window as any).indexedDB = window.indexedDB || window['mozIndexedDB'] || window['webkitIndexedDB'] || window['msIndexedDB'];
-    // tslint:disable-next-line:max-line-length
-    // (window as any).IDBTransaction = window['IDBTransaction'] || window['webkitIDBTransaction'] || window['msIDBTransaction'] || { READ_WRITE: 'readwrite' };
-    // (window as any).IDBKeyRange = window['IDBKeyRange'] || window['webkitIDBKeyRange'] || window['msIDBKeyRange'];
-
     if (!window.indexedDB) {
       throw new Error('Your browser doesn\'t support a stable version of IndexedDB.');
     }
@@ -30,7 +25,7 @@ export class IndexedDBHelper {
     return new Observable<IDBDatabase>((observer) => {
       let result = new Result();
 
-      if (!indexedDB) {
+      if (!config) {
         result.addError('required', 'config');
         observer.error(result);
         return;
@@ -43,6 +38,7 @@ export class IndexedDBHelper {
       }
 
       const request = window.indexedDB.open(`${this.storageConfig.keyPrefix}.${config.dbName}`, config.dbVersion);
+
       request.onerror = (ev: any) => {
         result.addError('Database error: ' + ev.target.errorCode);
         observer.error(result);
